b5b5f247635c79f1dc9b210bb251fdd753ce5b0d
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-29 01:12:16 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,7 +16,7 @@ class Artist
   end
   
   def songs 
-    song.all.select {|song| song.artist == self}
+    Song.all.select {|song| song.artist == self}
   end 
   
   self.find_or_create_by_name(name)
