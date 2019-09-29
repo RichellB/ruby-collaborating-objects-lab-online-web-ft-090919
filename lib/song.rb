8ee94f807d755c524b0feb904aeab225ea0d9370
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-29 01:44:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,7 +13,7 @@ class Song
   end
   
   def self.new_by_filename(filename)
-    artist, song = filename.split("-")
+    artist, song = filename.split(" - ")
     new_song = self.new(song) 
     new_song.artist_name = artist
     song_song  
