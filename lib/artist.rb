6b76f75a9335509accd913214a2e65f4d537bfcf
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-29 01:04:32 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -22,7 +22,7 @@ class Artist
   self.find_or_create_by_name(name)
    if !self.all.find {|person| person.name == name 
      song = Song.new(name)
-     add_song(name)
+     add_song(song)
   end
     
 end 
