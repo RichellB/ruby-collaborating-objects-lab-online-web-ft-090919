9bfb4a544b4e8f4f287563c8bbf27563c845fa05
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-29 01:28:59 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,7 +1,7 @@
 class MP3Importer
   attr_accessor :filename, :title 
   
-  def getfile(filename_lists)
+  def import(filename_lists)
     filename_lists.each {|filename| Song.new_by_filename(filename)}
   end 
 end
